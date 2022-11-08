ddd950eaf4bf2c398963cbb59cc784899cc72eac
juyeon1031 <113417188+juyeon1031@users.noreply.github.com>
2022-11-08 17:02:49 +0900

    05

---
+++ main2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	FILE* fp;
+	char str[30];
+
+	fp = fopen("sample.txt", "w");
+	
+	printf("input a word");
+	scanf("%s", str);
+	fprintf(fp, "%s\n", str);
+
+	printf("input a word");
+	scanf("%s", str);
+	fprintf(fp, "%s\n", str);
+
+	printf("input a word");
+	scanf("%s", str);
+	fprintf(fp, "%s\n", str);
+
+
+	//fclose
+	fclose(fp);
+	
+	return 0;
+}
